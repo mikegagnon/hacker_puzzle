cc4398d51ecfb72090424b40f683769b3e7fb125
Mike Gagnon <mikegagnon@gmail.com>
2013-03-15 13:12:22 -0700

    template

--- vuln_web_app.py
+++ vuln_web_app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'oh hai'
-
+    compute = request.args.get('compute', '')
+    return render_template('compute.html', computation=compute)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
